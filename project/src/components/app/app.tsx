abbd7f963ce9c65b77c671148eaeea9399e475cc
maremshaowa.nasti@mail.ru <maremshaowa.nasti@mail.ru>
2021-11-02 01:27:40 +0300

    Добавление основных компонентов

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+
+type AppScreenProps = {
+  nameFilm: string,
+  genreFilm: string,
+  yearFilm: number;
+}
+
+function App({nameFilm, genreFilm, yearFilm} : AppScreenProps): JSX.Element {
+  return (<MainScreen nameFilm={nameFilm} genreFilm={genreFilm} yearFilm={yearFilm}/>);
 }
 
 export default App;
