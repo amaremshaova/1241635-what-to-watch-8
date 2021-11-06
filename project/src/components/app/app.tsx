7840e2dfb89d0e0939d57483b39685be601f7236
maremshaowa.nasti@mail.ru <maremshaowa.nasti@mail.ru>
2021-11-06 23:39:05 +0300

    Добавление маршрутизации

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,13 +1,53 @@
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
+import SignIn from '../sign-in/sign-in';
+import AddingReview from '../adding-review/adding-review';
+import MyList from '../my-list/my-list';
+import Film from '../film/film';
+import Player from '../player/player';
+import NotFoundPage from '../not-found-page/not-found-page';
+import PrivateRoute from '../private-route/private-route';
+import {AppRoute, AuthorizationStatus} from '../../const';
+
 
 type AppScreenProps = {
-  nameFilm: string,
-  genreFilm: string,
-  yearFilm: number;
+  name: string,
+  genre: string,
+  year: number;
 }
 
-function App({nameFilm, genreFilm, yearFilm} : AppScreenProps): JSX.Element {
-  return (<MainScreen nameFilm={nameFilm} genreFilm={genreFilm} yearFilm={yearFilm}/>);
+function App({name, genre, year} : AppScreenProps): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <MainScreen nameFilm={name} genreFilm={genre} yearFilm={year}/>
+        </Route>
+        <Route exact path={AppRoute.Login}>
+          <SignIn />
+        </Route>
+        <Route exact path={AppRoute.AddingReview}>
+          <AddingReview/>
+        </Route>
+        <Route exact path={AppRoute.Film}>
+          <Film />
+        </Route>
+        <Route exact path={AppRoute.Player}>
+          <Player />
+        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.MyList}
+          render={() => <MyList/>}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
+        <Route>
+          <NotFoundPage/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
